f08369942d519f8555a3652a2755c44d95d1359d
Jacqueline1201 <jthai1201@gmail.com>
2023-11-15 17:55:34 -0800

    Realistic fall

--- Components/sparkles.js
+++ Components/sparkles.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     for (var i = 0; i < 100; i++) {
         pxs[i] = new Circle();
         pxs[i].reset();
+        pxs[i].setY(Math.random()*HEIGHT);
     }
     requestAnimationFrame(draw);
 });
@@ -38,36 +39,34 @@ function draw() {
 function Circle() {
     this.s = {
         ttl: 15000,
-        xmax: 5,
-        ymax: 2,
-        rmax: 17,
+        xmax: 2,
+        ymin: 2,
+        ymax: 0, // descent speed
+        rmax: 30, // size
         rt: 1,
-        xdef: 960,
-        ydef: 540,
-        xdrift: 2,
-        ydrift: 2,
-        random: true,
-        blink: true
+        xdef: WIDTH/2,
+        ydef: 1,
+        xrandom: true,
+        yrandom: false,
+        blink: false
     };
     this.reset = function() {
-        this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
-        this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
+        this.x = (this.s.xrandom ? WIDTH * Math.random() : this.s.xdef);
+        this.y = (this.s.yrandom ? HEIGHT * Math.random() : this.s.ydef);
         this.r = ((this.s.rmax - 1) * Math.random()) + 1;
         this.dx = (Math.random() * this.s.xmax) * (Math.random() < 0.5 ? -1 : 1);
-        this.dy = (Math.random() * this.s.ymax) * (Math.random() < 0.5 ? -1 : 1);
+        this.dy = (3) * (Math.random() < 0.5 ? -1 : -1);
         this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
         this.rt = Math.random() * this.hl;
         this.s.rt = Math.random() + 1;
         this.stop = Math.random() * 0.2 + 0.4;
-        this.s.xdrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
-        this.s.ydrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
     };
     this.fade = function() {
-        this.rt += this.s.rt;
+        if (this.s.blink) this.rt += this.s.rt;
     };
     this.draw = function() {
-        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt * -1;
-        else if (this.rt >= this.hl) this.reset();
+        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt*-1; // blinks
+        // else if (this.rt >= this.hl) this.reset(); //reset
         var newo = 1 - (this.rt / this.hl);
         con.beginPath();
         con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
@@ -82,9 +81,12 @@ function Circle() {
     };
     this.move = function() {
         this.x += (this.rt / this.hl) * this.dx;
-        this.y += (this.rt / this.hl) * this.dy;
+        this.y -= Math.max(0.2, (this.rt / this.hl))* this.dy;
         if (this.x > WIDTH || this.x < 0) this.dx *= -1;
-        if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+        // if (this.x > WIDTH || this.x < 0) this.dx *= -1;
+        // if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+        if (this.y > HEIGHT || this.y < 0) this.reset();
+        // if (this.y > HEIGHT) this.dy *= this.reset();
     };
     this.getX = function() {
         return this.x;
@@ -92,4 +94,7 @@ function Circle() {
     this.getY = function() {
         return this.y;
     };
+    this.setY = function(height) {
+        this.y = height;
+    }
 };
